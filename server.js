ea01f6ee75f78009039e24e26ee74718f6bb8347
Vincent Huynh <vincent.huynh123@gmail.com>
2020-09-23 21:19:40 -0400

    test

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get("/", (req, res) => res.send("API Works!"));
 app.use("/api/cropprices", require("./routes/api/cropprices"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
