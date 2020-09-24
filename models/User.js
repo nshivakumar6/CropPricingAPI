b2f7b35e71e822031b05fb27b77f7b345e1edd97
Vincent Huynh <vincent.huynh123@gmail.com>
2020-09-24 18:24:24 -0400

    testing new .gitignore

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  level: {
+    type: Number,
+    default: 0,
+  },
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
