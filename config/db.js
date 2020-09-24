7e3ed5e3bc32aaf084202b42584fc16fcfee488a
Vincent Huynh <vincent.huynh123@gmail.com>
2020-09-24 18:12:28 -0400

    Added user delete route

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-      useFindAndModify: true,
+      useFindAndModify: false,
     });
     console.log("-- Connected to MongoDB --");
   } catch (err) {
