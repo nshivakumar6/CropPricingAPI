c5ced7de3dd1d85338fd04e155f0b7721e8717bd
Vincent Huynh <vincent.huynh123@gmail.com>
2021-04-23 11:34:44 -0400

    Removed config lines in multiple files, removed config from project, removed test route branch, fixed GET by Market, Product, Variety, and/or Category route to use wildcard for search

--- routes/api/cropprices.js
+++ routes/api/cropprices.js
@@ -1,171 +1,171 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
-const pool = require("../../config/db");
+const { BigQuery } = require('@google-cloud/bigquery');
+const bigquery = new BigQuery();
 
 // @route     GET /api/cropprices/product/all
 // @desc      GET ALL crop pricing listing
 // @access    Public (this will be the case for all until we have some form of auth)
 router.get('/all', async (req, res) => {
-  const { limit } = req.body;
-  let query = 'SELECT * FROM price';
-  let queryArgs = [];
-  if (limit) {
-    query = query.concat(' limit ($1)');
-    queryArgs.push(limit)
-  }
-  try {
-    pool.query(
-      query,
-      queryArgs,
-      (err, qres) => {
-        if (err) {
-          throw err;
-        }
-        res.status(200).json(qres.rows);
-      }
-    )
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
+    const { limit } = req.body;
+    const query = `SELECT * FROM \`croppricing.CropPricing.prices\``;
+    let options = {
+        query: query,
+        location: 'US',
+    }
+
+    if (limit) {
+        options.query = query.concat(' LIMIT @limit');
+        options.params = {limit: limit}
+    }
+
+    try {
+        const [job] = await bigquery.createQueryJob(options);
+        const [rows] = await job.getQueryResults();
+        res.status(200).json(rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 })
 
 // @route     GET /api/cropprices/product
 // @desc      GET crop pricing listing by product, enter product name in request body as 'product'
 // @access    Public (this will be the case for all until we have some form of auth)
 router.get('/product', [
-  check('product', 'Product name required').not().isEmpty(),
-], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({errors: errors.array()});
-  }
-  const { product, limit } = req.body;
-  let query = 'SELECT * FROM price WHERE LOWER(product) like LOWER($1)';
-  let queryArgs = ['%' + product + '%'];
-  if (limit) {
-    query = query.concat(' limit ($2)');
-    queryArgs.push(limit)
-  }
-  try {
-    pool.query(
-      query,
-      queryArgs,
-      (err, qres) => {
-        if (err) {
-          throw err;
-        }
-        res.status(200).json(qres.rows);
-      }
-    )
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
+    check('product', 'Product name required').not().isEmpty(),
+  ], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()});
+    }
+    const { product, limit } = req.body;
+    const query = 
+    `SELECT * FROM \`croppricing.CropPricing.prices\` WHERE LOWER(product) like LOWER(@product)`;
+    let options = {
+        query: query,
+        location: 'US',
+        params: {product: '%' + product + '%'}
+    }
+
+    if (limit) {
+        options.query = query.concat(' LIMIT @limit');
+        options.params.limit = limit
+    }
+    try {
+        const [job] = await bigquery.createQueryJob(options);
+        // console.log(`Job ${job.id} started.`);
+        const [rows] = await job.getQueryResults();
+        res.status(200).json(rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 })
 
 // @route     GET /api/cropprices/variety
 // @desc      GET crop pricing listing by variety, enter variety name in request body as 'variety'
 // @access    Public (this will be the case for all until we have some form of auth)
 router.get('/variety', [
-  check('variety', 'Variety name required').not().isEmpty(),
-], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({errors: errors.array()});
-  }
-  const { variety, limit } = req.body;
-  let query = 'SELECT * FROM price WHERE LOWER(variety) like LOWER($1)';
-  let queryArgs = ['%' + variety + '%'];
-  if (limit) {
-    query = query.concat(' limit ($2)');
-    queryArgs.push(limit)
-  }
-  try {
-    pool.query(
-      query,
-      queryArgs,
-      (err, qres) => {
-        if (err) {
-          throw err;
-        }
-        res.status(200).json(qres.rows);
-      }
-    )
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
+    check('variety', 'Variety name required').not().isEmpty(),
+  ], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()});
+    }
+    const { variety, limit } = req.body;
+
+    const query = `SELECT * FROM \`croppricing.CropPricing.prices\` WHERE LOWER(variety) LIKE @variety`;
+    let options = {
+        query: query,
+        location: 'US',
+        params: {variety: '%' + variety + '%'}
+    }
+
+    if (limit) {
+        options.query = query.concat(' LIMIT @limit');
+        options.params.limit = limit
+    }
+    try {
+        const [job] = await bigquery.createQueryJob(options);
+        // console.log(`Job ${job.id} started.`);
+        const [rows] = await job.getQueryResults();
+        res.status(200).json(rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 })
 
 // @route     GET /api/cropprices/category
 // @desc      GET crop pricing listing by category, enter category name in request body as 'category'
 // @access    Public (this will be the case for all until we have some form of auth)
 router.get('/category', [
-  check('category', 'Category name required').not().isEmpty(),
-], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({errors: errors.array()});
-  }
-  const { category, limit } = req.body;
-  let query = 'SELECT * FROM price WHERE LOWER(category) like LOWER($1)';
-  let queryArgs = ['%' + category + '%'];
-  if (limit) {
-    query = query.concat(' limit ($2)');
-    queryArgs.push(limit)
-  }
-  try {
-    pool.query(
-      query,
-      queryArgs,
-      (err, qres) => {
-        if (err) {
-          throw err;
-        }
-        res.status(200).json(qres.rows);
-      }
-    )
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
+    check('category', 'Variety name required').not().isEmpty(),
+  ], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()});
+    }
+    const { category, limit } = req.body;
+
+    const query = `SELECT * FROM \`croppricing.CropPricing.prices\` WHERE LOWER(category) LIKE @category`;
+    let options = {
+        query: query,
+        location: 'US',
+        params: {category: '%' + category + '%'}
+    }
+
+    if (limit) {
+        options.query = query.concat(' LIMIT @limit');
+        options.params.limit = limit
+    }
+    try {
+        const [job] = await bigquery.createQueryJob(options);
+        // console.log(`Job ${job.id} started.`);
+        const [rows] = await job.getQueryResults();
+        res.status(200).json(rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 })
 
 // @route     GET /api/cropprices/market
 // @desc      GET crop pricing listing by market, enter market name in request body as 'market'
 // @access    Public (this will be the case for all until we have some form of auth)
 router.get('/market', [
-  check('market', 'Market name required').not().isEmpty(),
-], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({errors: errors.array()});
-  }
-  const { market, limit } = req.body;
-  let query = 'SELECT * FROM price WHERE LOWER(market) like LOWER($1)';
-  let queryArgs = ['%' + market + '%'];
-  if (limit) {
-    query = query.concat(' limit ($2)');
-    queryArgs.push(limit)
-  }
-  try {
-    pool.query(
-      query,
-      queryArgs,
-      (err, qres) => {
-        if (err) {
-          throw err;
-        }
-        res.status(200).json(qres.rows);
-      }
-    )
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
+    check('market', 'Variety name required').not().isEmpty(),
+  ], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()});
+    }
+    const { market, limit } = req.body;
+
+    const query = `SELECT * FROM \`croppricing.CropPricing.prices\` WHERE LOWER(market) LIKE @market`;
+    let options = {
+        query: query,
+        location: 'US',
+        params: {market: '%' + market + '%'}
+    }
+
+    if (limit) {
+        options.query = query.concat(' LIMIT @limit');
+        options.params.limit = limit
+    }
+    try {
+        const [job] = await bigquery.createQueryJob(options);
+        // console.log(`Job ${job.id} started.`);
+        const [rows] = await job.getQueryResults();
+        res.status(200).json(rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 })
+
 
 // @route     GET /api/cropprices/
 // @desc      GET anything based on parameters in the request body
@@ -175,51 +175,47 @@ router.get('/market', [
 //            (also note that limit MUST be the last argument in the request body)
 // @access    Public (this will be the case for all until we have some form of auth)
 router.get('/', async (req, res) => {
-  const args = { category, variety, product, market} = req.body;
-  const { limit } = req.body;
-  if (args && Object.keys(args).length === 0 && args.constructor === Object) {
-    
-    return res.status(400).json({errors: [{
-      "msg": "Category, variety, product, or market required",
-      "param": "any",
-      "location": "body"
-    }]});
-  }
-  let queryIndex = 1;
-  let query = 'SELECT * FROM price WHERE ';
-  let queryArgs = [];
-  
-  for (key in args) {
-    if (queryIndex == 1) {
-      query = query.concat(`LOWER(${key}) like LOWER($${queryIndex})`);
-      queryArgs.push('%' + args[key] + '%');
-    } else if (key === 'limit') {
-      query = query.concat(` limit ($${queryIndex})`);
-      queryArgs.push(`${args[key]}`)
+    const args = { category, variety, product, market} = req.body;
+    const { limit } = req.body;
+    if (args && Object.keys(args).length === 0 && args.constructor === Object) {
+      
+      return res.status(400).json({errors: [{
+        "msg": "Category, variety, product, or market required",
+        "param": "any",
+        "location": "body"
+      }]});
     }
-    else {
-      query = query.concat(` AND LOWER(${key}) like LOWER($${queryIndex})`);
-      queryArgs.push('%' + args[key] + '%');
-    }
-    queryIndex++;
-  }
-  console.log("Query", query);
-  console.log("args:", queryArgs);
-  try {
-    pool.query(
-      query,
-      queryArgs,
-      (err, qres) => {
-        if (err) {
-          throw err;
-        }
-        res.status(200).json(qres.rows);
-      }
-    )
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-})
 
+    let query = `SELECT * FROM \`croppricing.CropPricing.prices\` WHERE `;
+    let options = {
+        query: query,
+        location: 'US',
+        params: {}
+    }
+    let count = 0;
+    for (a in args) {
+        if (count == 0) {
+            options.query = options.query.concat(`LOWER(${a}) like LOWER(@${a}) `);
+        } else if (a == "limit") {
+            continue;  
+        } else {
+            options.query = options.query.concat(`AND LOWER(${a}) like LOWER(@${a}) `);
+        }
+        options.params[a] = '%' + args[a] + '%';
+        count++;
+    }
+    if (limit) {
+        options.query = options.query.concat('LIMIT @limit');
+        options.params.limit = limit
+    }
+    try {
+        const [job] = await bigquery.createQueryJob(options);
+        // console.log(`Job ${job.id} started.`);
+        const [rows] = await job.getQueryResults();
+        res.status(200).json(rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require('dotenv').config();
-const dbPool = require('./config/db');
 const app = express();
 
 app.use(express.json({ extended: false }));
@@ -9,7 +8,6 @@ app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("You've reached the Crop Pricing API!"));
 
 app.use("/api/cropprices", require("./routes/api/cropprices"));
-app.use("/api/test", require("./routes/api/test"));
 
 const PORT = process.env.PORT || 5000;
 
